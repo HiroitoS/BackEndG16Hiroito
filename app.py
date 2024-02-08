27a5cdc2a8064d1ae745f32fa78577d277762de2
HiroitoS <hitoito.sanchez@gmail.com>
2024-02-07 19:21:42 -0500

    Terminando semana 04

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from os import environ
 from models import *
 from controllers import *
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
 
 
 
@@ -18,16 +19,22 @@ load_dotenv()
 
 app= Flask(__name__)
 api = Api(app=app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
 
 conexion.init_app(app)
 
+JWTManager(app=app)
 # Esto crea la utilizacion de las migraciones en nuestro proyecto de flask
 Migrate(app=app, db=conexion)
 
 # Agrego mis rutas
 
 api.add_resource(InvitadosController, '/invitados')
+api.add_resource(BarmanController, '/barman')
+api.add_resource(LoginController, '/login')
+
 
 
 if __name__ == "__main__":
